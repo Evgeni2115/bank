d12d740216520c162ea49d12f90a558df2767121
Евгений Решетников <reshetnikov_evgeni@mail.ru>
2023-06-11 13:56:04 +0000

    подключил общий рест-хендлер в transfer[bk-1]

--- transfer/src/main/java/com/bank/transfer/TransferApplication.java
+++ transfer/src/main/java/com/bank/transfer/TransferApplication.java
@@ -1,11 +1,13 @@
 package com.bank.transfer;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+
 @EnableEurekaClient
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.bank.common", "com.bank.transfer"})
 public class TransferApplication {
     public static void main(String[] args) {
         SpringApplication.run(TransferApplication.class, args);
